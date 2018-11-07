6d9b78a818dd2f06dfb321d8bfd8b7e64f522602
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2018-11-07 09:55:14 -0800

    added solitaire app

---
+++ solitaire/precache-manifest.0fac16349a10bea51a718111ab58041e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "203561e27827eb60ff0b",
+    "url": "/static/js/main.203561e2.chunk.js"
+  },
+  {
+    "revision": "ac7ae7041184cab67d11",
+    "url": "/static/js/1.ac7ae704.chunk.js"
+  },
+  {
+    "revision": "203561e27827eb60ff0b",
+    "url": "/static/css/main.5dceb2e7.chunk.css"
+  },
+  {
+    "revision": "bdbe11802026a1348109dc2931db04fa",
+    "url": "/index.html"
+  }
+];
