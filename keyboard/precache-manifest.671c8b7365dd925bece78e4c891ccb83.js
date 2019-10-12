9de85003cf14c849b88f111c338f39eedcc14b55
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2019-10-11 19:11:09 -0700

    set base app url for keyboard

---
+++ keyboard/precache-manifest.671c8b7365dd925bece78e4c891ccb83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a965412a5bf93af2b58983b1f25bfa81",
+    "url": "/keyboard/index.html"
+  },
+  {
+    "revision": "aea5513f51fabfed272d",
+    "url": "/keyboard/static/css/main.23ebef90.chunk.css"
+  },
+  {
+    "revision": "87fcb094bf064117092d",
+    "url": "/keyboard/static/js/2.16ad88cf.chunk.js"
+  },
+  {
+    "revision": "aea5513f51fabfed272d",
+    "url": "/keyboard/static/js/main.337287fd.chunk.js"
+  },
+  {
+    "revision": "97af16afc73d52e35237",
+    "url": "/keyboard/static/js/runtime-main.3561f81d.js"
+  }
+]);
--- keyboard/service-worker.js
+++ keyboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcc100bc140eb33486cee7ba64bada8f.js"
+  "/keyboard/precache-manifest.671c8b7365dd925bece78e4c891ccb83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keyboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
